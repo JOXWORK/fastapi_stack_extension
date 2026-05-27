6f7864a88d81f0f0ee9dd6ad450d41121f29f05b
JOXWORK <joxwork1995@gmail.com>
2026-05-27 16:47:52 +0500

    create bearer transport

--- application/core/authentication/__init__.py
+++ application/core/authentication/__init__.py
@@ -1,7 +1,9 @@
 from .get_user_manager import get_user_manager
+from .transport import bearer_transport
 from .user_manager import UserManager
 
 __all__ = (
     "UserManager",
     "get_user_manager",
+    "bearer_transport",
 )
