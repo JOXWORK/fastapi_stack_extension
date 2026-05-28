dd37293022337381d7ed47ef421452409ab68d25
JOXWORK <joxwork1995@gmail.com>
2026-05-28 14:17:37 +0500

    create fastapi_users strategy

---
+++ application/api/dependencies/auth/strategy.py
@@ -0,0 +1,13 @@
+from core.config import settings
+from core.models import AccessToken, get_access_token_db
+from fastapi import Depends
+from fastapi_users.authentication.strategy.db import AccessTokenDatabase, DatabaseStrategy
+
+
+def get_database_strategy(
+    access_token_db: AccessTokenDatabase[AccessToken] = Depends(get_access_token_db),
+) -> DatabaseStrategy:
+    return DatabaseStrategy(
+        database=access_token_db,
+        lifetime_seconds=settings.auth.access_token.lifetime_seconds,
+    )
