0750239a5d8661fbab1c12eb679ed40e1d55682e
JOXWORK <joxwork1995@gmail.com>
2026-05-30 10:58:53 +0500

    remove 'get_user_manager' dependencies from core

--- application/core/authentication/get_user_manager.py
+++
@@ -1,9 +0,0 @@
-from fastapi import Depends
-
-from core.models import get_user_db
-
-from .user_manager import UserManager
-
-
-async def get_user_manager(user_db=Depends(get_user_db)):
-    yield UserManager(user_db=user_db)
