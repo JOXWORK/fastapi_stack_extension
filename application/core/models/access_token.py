8a22171563edcead30f236f00d2c335da285f5dc
JOXWORK <joxwork1995@gmail.com>
2026-05-30 11:00:18 +0500

    create classmethods that returns root of the depends

---
+++ application/core/models/access_token.py
@@ -0,0 +1,31 @@
+from fastapi_users.db import SQLAlchemyUserDatabase
+from fastapi_users_db_sqlalchemy.access_token import (
+    AsyncSession,
+    Mapped,
+    SQLAlchemyBaseAccessTokenTable,
+    String,
+    mapped_column,
+)
+from sqlalchemy import ForeignKey
+
+from core.types_own.user_id import UserIdType
+
+from .base import Base
+
+
+class AccessToken(SQLAlchemyBaseAccessTokenTable[UserIdType], Base):
+    token: Mapped[str] = mapped_column(
+        String(length=43),
+        primary_key=True,
+        unique=True,
+    )
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey(
+            column="users.id",
+            ondelete="cascade",
+        )
+    )
+
+    @classmethod
+    def get_db(cls, session: AsyncSession):
+        return SQLAlchemyUserDatabase(session=session, user_table=cls)
---
+++ application/core/models/user.py
@@ -0,0 +1,21 @@
+from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
+from sqlalchemy import String
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import Mapped, mapped_column
+
+from core.models.mixins import BaseIntIdPkMixin
+from core.types_own.user_id import UserIdType
+
+from .base import Base
+
+
+class User(
+    Base,
+    SQLAlchemyBaseUserTable[UserIdType],
+    BaseIntIdPkMixin,
+):
+    name: Mapped[str | None] = mapped_column(String(50))
+
+    @classmethod
+    def get_db(cls, session: AsyncSession):
+        return SQLAlchemyUserDatabase(session=session, user_table=cls)
