8eda8379b220c324a4a38e8aac9e564e1d60ef68
JOXWORK <joxwork1995@gmail.com>
2026-05-30 10:53:37 +0500

    implementation 'BaseIntIdPkMixin' into 'Example' model

--- application/core/models/example.py
+++ application/core/models/example.py
@@ -1,9 +1,11 @@
 from sqlalchemy.orm import Mapped, mapped_column
 
+from core.models.mixins import BaseIntIdPkMixin
+
 from .base import Base
 
 
-class Example(Base):
+class Example(Base, BaseIntIdPkMixin):
     example: Mapped[str] = mapped_column(
         default="example",
         server_default="example",
