55c00ad1838f8499dc4abfdabb0064345ae92448
JOXWORK <joxwork1995@gmail.com>
2026-06-03 09:26:40 +0500

    refactor: change v1 endpoint py file name 'auth' -> 'authentication'

--- application/api/v1/__init__.py
+++ application/api/v1/__init__.py
@@ -1,7 +1,7 @@
 from core.config import settings
 from fastapi import APIRouter
 
-from .authentication.auth import router as authentication_router
+from .authentication.authentication import router as authentication_router
 from .hello_world.views import router as hello_world_router
 
 router = APIRouter(prefix=settings.api.v1.prefix.router_v1)
