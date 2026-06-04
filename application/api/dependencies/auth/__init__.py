1252bfe4a1d21ef26aa7ce5820d82f8d0458183b
JOXWORK <joxwork1995@gmail.com>
2026-06-04 09:15:16 +0500

    add: init file imports

--- application/api/dependencies/auth/__init__.py
+++ application/api/dependencies/auth/__init__.py
@@ -1,7 +1,10 @@
+from .fastapi_users_instance import fastapi_current_superuser, fastapi_current_user
 from .get_user_db import get_user_db
 from .get_user_manager import get_user_manager
 
 __all__ = (
     "get_user_db",
     "get_user_manager",
+    "fastapi_current_user",
+    "fastapi_current_superuser",
 )
