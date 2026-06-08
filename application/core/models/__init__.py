d6c0150039286dab0b9aa4374af40c6bb5b8e64e
JOXWORK <joxwork1995@gmail.com>
2026-06-08 10:13:23 +0500

    add: session_id foreign key column

--- application/core/models/__init__.py
+++ application/core/models/__init__.py
@@ -3,10 +3,11 @@
 from .base import Base
 from .user import User
 from .example import Example
+from .user_session import UserSession
 from .access_token import AccessToken
 from .db_attach import db_attach
-from .user_session import UserSession
 from .refresh_token import RefreshToken
+
 
 __all__ = (
     "db_attach",
--- application/core/models/access_token.py
+++ application/core/models/access_token.py
@@ -18,9 +18,17 @@ class AccessToken(Base, SQLAlchemyBaseAccessTokenTable[UserIdType]):
         String(length=43),
         primary_key=True,
     )
+
     user_id: Mapped[int] = mapped_column(
         ForeignKey(
             column="users.id",
+            ondelete="cascade",
+        )
+    )
+
+    session_id: Mapped[int] = mapped_column(
+        ForeignKey(
+            column="user_sessions.id",
             ondelete="cascade",
         )
     )
