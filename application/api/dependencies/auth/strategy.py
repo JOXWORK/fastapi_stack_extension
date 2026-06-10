57a16a0e0b8149c2978102cecc2af3597a3d4835
JOXWORK <joxwork1995@gmail.com>
2026-06-10 14:53:22 +0500

    implement: custom strategy

--- application/api/dependencies/auth/strategy.py
+++ application/api/dependencies/auth/strategy.py
@@ -1,15 +1,17 @@
+from core.authentication.strategy_own import StrategyOwn
 from core.config import settings
-from core.models import AccessToken
+from core.models import AccessToken, UserSession
 from fastapi import Depends
-from fastapi_users.authentication.strategy.db import AccessTokenDatabase, DatabaseStrategy
+from fastapi_users.authentication.strategy.db import AccessTokenDatabase
 
 from .get_access_token_db import get_access_token_db
 
 
 def get_database_strategy(
     access_token_db: AccessTokenDatabase[AccessToken] = Depends(get_access_token_db),
-) -> DatabaseStrategy:
-    return DatabaseStrategy(
+) -> StrategyOwn:
+    return StrategyOwn(
+        user_session=UserSession,
         database=access_token_db,
         lifetime_seconds=settings.auth.access_token.lifetime_seconds,
     )
