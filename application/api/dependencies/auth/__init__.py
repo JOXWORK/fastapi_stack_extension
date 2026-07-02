db88a2f4e77056248ab829310ee3407cd1667488
JOXWORK <joxwork1995@gmail.com>
2026-07-02 13:05:02 +0500

    add: get_token_catcher depend into module init file

--- application/api/dependencies/auth/__init__.py
+++ application/api/dependencies/auth/__init__.py
@@ -1,4 +1,5 @@
 from .fastapi_users_instance import fastapi_current_superuser, fastapi_current_user
+from .get_token_catcher import get_token_catcher
 from .get_user_db import get_user_db
 from .get_user_manager import get_user_manager
 
@@ -7,4 +8,5 @@ __all__ = (
     "get_user_manager",
     "fastapi_current_user",
     "fastapi_current_superuser",
+    "get_token_catcher",
 )
