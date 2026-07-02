c5c4c307dc79217544f3aeba9e8fea4f28042935
JOXWORK <joxwork1995@gmail.com>
2026-07-02 13:02:31 +0500

    add: token catcher + own access token db into module init file

--- application/core/authentication/own/__init__.py
+++ application/core/authentication/own/__init__.py
@@ -1,13 +1,17 @@
+from .access_token_own import SQLAlchemyAccessTokenDatabaseOwn
 from .backend_own import AuthenticationBackendOwn
 from .bearer_own import BearerTransportOwn
 from .refresh_token_database import RefreshTokenDatabase
 from .strategy_own import StrategyOwn
+from .token_catcher import token_catcher
 from .user_session_database import UserSessionDatabase
 
 __all__ = (
+    "SQLAlchemyAccessTokenDatabaseOwn",
     "StrategyOwn",
     "UserSessionDatabase",
     "AuthenticationBackendOwn",
     "BearerTransportOwn",
     "RefreshTokenDatabase",
+    "token_catcher",
 )
