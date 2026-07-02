a862cb38d77e9412536c5fd4e223d504916e65af
JOXWORK <joxwork1995@gmail.com>
2026-07-02 12:57:25 +0500

    create: modified access token database

---
+++ application/core/authentication/own/access_token_own.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from fastapi_users_db_sqlalchemy.access_token import SQLAlchemyAccessTokenDatabase
+from sqlalchemy import select
+
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
+
+    from core.models import AccessToken
+
+
+class SQLAlchemyAccessTokenDatabaseOwn(
+    SQLAlchemyAccessTokenDatabase,
+):
+    def __init__(self, session, access_token_table):
+        super().__init__(session, access_token_table)
+
+        self.session: AsyncSession
+        self.access_token_table: AccessToken
+
+    async def get_token_ignore_expire(self, token: str) -> AccessToken | None:
+        query = select(self.access_token_table).where(self.access_token_table.token == token)
+        result = await self.session.execute(query)
+        return result.scalar_one_or_none()
