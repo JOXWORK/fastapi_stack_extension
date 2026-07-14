a34eaf76ad1f3c0785e17952f034166abfc153a1
JOXWORK <joxwork1995@gmail.com>
2026-07-14 19:09:48 +0500

    implement: own user manager class inheritance

--- application/core/authentication/user_manager.py
+++ application/core/authentication/user_manager.py
@@ -2,8 +2,9 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from fastapi_users import BaseUserManager, IntegerIDMixin
+from fastapi_users import IntegerIDMixin
 
+from core.authentication.own import BaseUserManagerOwn
 from core.config import settings
 from core.models import User
 from core.types_own.user_id import UserIdType
@@ -14,7 +15,7 @@ if TYPE_CHECKING:
     from fastapi import Request
 
 
-class UserManager(IntegerIDMixin, BaseUserManager[User, UserIdType]):
+class UserManager(IntegerIDMixin, BaseUserManagerOwn[User, UserIdType]):
     reset_password_token_secret = settings.auth.access_token.reset_password_token_secret
     verification_token_secret = settings.auth.access_token.verification_token_secret
 
