c828d70ed19cd4de401850de9c2469fb4a8ebf45
JOXWORK <joxwork1995@gmail.com>
2026-07-19 13:34:45 +0500

    create exceptions: refresh token exceptions

--- application/core/authentication/own/exceptions_own.py
+++ application/core/authentication/own/exceptions_own.py
@@ -3,3 +3,15 @@ from fastapi_users.exceptions import FastAPIUsersException
 
 class UserSessionInvalid(FastAPIUsersException):
     pass
+
+
+class RefreshTokenRevoked(FastAPIUsersException):
+    pass
+
+
+class RefreshTokenUsed(FastAPIUsersException):
+    pass
+
+
+class RefreshTokenNotExeists(FastAPIUsersException):
+    pass
