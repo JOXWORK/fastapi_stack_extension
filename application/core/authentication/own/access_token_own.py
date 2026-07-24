a883c73922cc4e4d582985780b21e6e7d2c47897
JOXWORK <joxwork1995@gmail.com>
2026-07-24 22:18:56 +0500

    add method: get_by_user_session_id

--- application/core/authentication/own/access_token_own.py
+++ application/core/authentication/own/access_token_own.py
@@ -8,7 +8,7 @@ from sqlalchemy import select
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
 
-    from core.models import AccessToken
+    from core.models.access_token import AccessToken
 
 
 class SQLAlchemyAccessTokenDatabaseOwn(
@@ -22,5 +22,10 @@ class SQLAlchemyAccessTokenDatabaseOwn(
 
     async def get_token_ignore_expire(self, token: str) -> AccessToken | None:
         query = select(self.access_token_table).where(self.access_token_table.token == token)
+        result = await self.session.execute(query)
+        return result.scalar_one_or_none()
+
+    async def get_by_user_session_id(self, session_id: int) -> AccessToken | None:
+        query = select(self.access_token_table).where(self.access_token_table.session_id == session_id)
         result = await self.session.execute(query)
         return result.scalar_one_or_none()
