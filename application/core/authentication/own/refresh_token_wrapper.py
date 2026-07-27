8fe06fb8485c4db7c4e7e6a883bc414bdce6da61
JOXWORK <joxwork1995@gmail.com>
2026-07-27 13:39:10 +0500

    deprecate: refresh token wrapper implement: dict as refresh token wrapper

--- application/core/authentication/own/refresh_token_wrapper.py
+++
@@ -1,7 +0,0 @@
-from core.models.refresh_token import RefreshToken
-
-
-class RefreshTokenWrapper:
-    def __init__(self, model: RefreshToken, token: str):
-        self.model = model
-        self.token = token
