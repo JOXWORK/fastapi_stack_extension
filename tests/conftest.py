a954675baff2f8b7c0f486e8c2f58ca538dd29f5
JOXWORK <joxwork1995@gmail.com>
2026-08-08 17:32:09 +0500

    add: register fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,15 +16,36 @@ async def client():
 
 
 @pytest_asyncio.fixture
-async def token_pair(client: AsyncClient, login_credentials: dict):
+async def register(client: AsyncClient):
+    response = await client.post(
+        url=url_root + "/auth/register",
+        json=credentials["register"],
+    )
+
+    print(credentials["register"])
+
+    assert response.status_code == 201
+
+
+@pytest_asyncio.fixture(scope="session")
+async def token_pair(client: AsyncClient):
     response = await client.post(
         url=url_root + "/auth/login",
-        data=login_credentials,
+        data=credentials["login"],
     )
 
     assert response.status_code == 200
 
     data = orjson.loads(response.text)
 
+    print(data)
+
     assert "access_token" in data
     assert "refresh_token" in data
+
+    return data
+
+
+@pytest_asyncio.fixture
+async def first_register_login(register, token_pair):
+    pass
